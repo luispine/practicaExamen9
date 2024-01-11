343200a1fa1f90aa854e45583f614eadb9623ea9
luiVaz <vazquezcedar@outlook.com>
2024-01-10 23:00:01 -0600

    Integrar pruebas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -8,7 +8,7 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create tthe app', () => {
+  it('shouuuld create tthe app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
